183784021687e8b81154a4ba0afa847a26996d87
shaoyingjian <cleanairdoris@gmail.com>
2017-06-21 22:28:24 +0800

    增加接口

--- src/main/main.go
+++ src/main/main.go
@@ -7,6 +7,9 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+
+	"flag"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -33,10 +36,29 @@ type ContentSlice struct {
 	Contents []Content `json:"contents"`
 }
 
+type Configs struct {
+	DbType string `json:"dbtype"`
+	User   string `json:"user"`
+	Pwd    string `json:"pwd"`
+	DbName string `json:"dbname"`
+}
+
 var db = &sql.DB{}
 
 func init() {
-	db, _ = sql.Open("mysql", "root:6579.?a@tcp(localhost:3306)/one?charset=utf8")
+	configFile := flag.String("cfg", "config.json", "the db config filename")
+	fi, err := os.Open(*configFile)
+	fmt.Println(*configFile)
+	if err != nil {
+		panic(err)
+	}
+	defer fi.Close()
+	buf, _ := ioutil.ReadAll(fi)
+	var config Configs
+	json.Unmarshal(buf, &config)
+
+	//db, _ = sql.Open("mysql", "root:6579.?a@tcp(localhost:3306)/one?charset=utf8")
+	db, _ = sql.Open(config.DbType, config.User+":"+config.Pwd+"@tcp(localhost:3306)/"+config.DbName+"?charset=utf8")
 }
 
 func getTitle(w http.ResponseWriter, r *http.Request) {
